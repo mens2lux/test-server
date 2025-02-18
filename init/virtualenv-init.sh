435b3cfde03a8ef79c69525f62f66597414fed29
mens2lux <14057404+mens2lux@users.noreply.github.com>
2025-02-18 17:24:42 +0100

    Update virtualenv-init.sh

--- init/virtualenv-init.sh
+++ init/virtualenv-init.sh
@@ -1,5 +1,5 @@
 sudo apt install python3-virtualenv python3-pip -y
 mkdir .venvs
-python3 -m virtualenv .venvs/server
-echo 'source .venvs/server/bin/activate' >> .bashrc
+python3 -m virtualenv .venvs/test-server
+echo 'source .venvs/test-server/bin/activate' >> .bashrc
 source .bashrc
